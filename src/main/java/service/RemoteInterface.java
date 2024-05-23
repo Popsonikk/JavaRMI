2e348d21085d47d9e95cf683290a6a7ca8ecbd1a
Popsonikk <popson2001@onet.pl>
2024-05-23 11:09:13 +0200

    Połączenie w jeden projekt

---
+++ src/main/java/service/RemoteInterface.java
@@ -0,0 +1,10 @@
+package service;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RemoteInterface extends Remote {
+
+    boolean register(String name, String password, boolean isAdmin) throws RemoteException;
+    boolean logIn(String name, String password);
+}
